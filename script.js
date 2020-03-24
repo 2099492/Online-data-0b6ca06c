f72929c26620897c199e98c99bdf4e84cc205818
2099492 <2099492@talnet.com>
2020-03-24 11:57:05 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,3 @@
+var object = {"name": "Zara", "score": 36, "Begin datum": "21 Januari 2016", 'vrienden': ["Mika", "Josef", "Maria", "Sumail", "Lotte"], "Gekwalificeerd": true};
+
+console.log(object);
